57ee2174b8f4118ea32d2d61201e70fd7cf5920e
Alexandr <50238837+quasar46@users.noreply.github.com>
2022-11-11 20:17:06 +0300

    update files

---
+++ assets/lang.js
@@ -0,0 +1,62 @@
+const langArr = {
+  "description-subtitle": {
+    en: 'Introducing Blockchain Domain Name Service (BDNS)',
+    cn: 'Einführung von Blockchain Domain Name Service (BDNS)',
+    es: 'Introduciendo el Servicio de Nombres de Dominio Blockchain (BDNS)',
+  },
+  "description-text1": {
+    en: `BDNS is the world's first Ethereum name-only domain name service. BDNS are mintable in the form non-fungible tokens (NFTs) which you can configure to an Ethereum wallet address on a purely name-only basis. For example, instead of "Blockchain.eth", your Ethereum wallet address becomes simply "Blockchain" with the purchase and configuration of a BDNS NFT.`,
+    cn: 'BDNS ist der weltweit erste Ethereum Name-Only-Domain (NOD) Anbieter. NODs sind reine "Name-Only" Wallet-Adressen und können in Form von Non-Fungible-Tokens (NFTs) gemint werden, die Sie für jede Ethereum-Wallet konfigurieren können. Zum Beispiel wird Ihre Ethereum-Wallet-Adresse statt "Blockchain.eth" einfach "Blockchain" mit dem Kauf und der Konfiguration eines BDNS NFTs.',
+    es: 'BDNS es el primer proveedor Mundial de dominios de sólo nombre (Name-Only-Domains) (NOD) en Ethereum. Los NODs son direcciones de monedero de sólo nombre y son minables en forma de tokens no fungibles (NFTs) que puedes configurar en cualquier monedero de Ethereum. Por ejemplo, en lugar de "Blockchain.eth", la dirección de su cartera de Ethereum se convierte simplemente en "Blockchain" con la compra y configuración de un BDNS NFT.',
+  },
+  "advantages-item1-title": {
+    en: 'Location, Location, Location!',
+    cn: 'Standort, Standort, Standort!',
+    es: 'Localización, localización, localización',
+  },
+  "advantages-text1": {
+    en: `In the real world economy, addresses matter - and that's true for the virtual world economy too. NODs elicit the potential to configure your Ethereum wallet address to the most prestigious names on the Blockchain. Over time, the exclusivity of a NOD will translate into a more prestigious wallet address than any other on the Blockchain, the same way as a Tier 1 city address has done so in the physical world`,
+    cn: 'In der realen Wirtschaft spielen Adressen eine Rolle - und das gilt auch für die virtuelle Weltwirtschaft. NODs bieten die Möglichkeit, Ihre Ethereum-Wallet-Adresse mit den prestigeträchtigsten Namen auf der Blockchain zu verbinden. Mit der Zeit wird sich die Exklusivität einer NOD in eine prestigeträchtigere Wallet-Adresse als jede andere auf der Blockchain verwandeln, genauso wie eine Stufe-1-Stadtadresse dies in der physischen Welt getan hat.',
+    es: 'En la economía del mundo real, las direcciones importan - y eso es cierto para la economía del mundo virtual también. Los NODs eligen el potencial para configurar la dirección de tu cartera de Ethereum con los nombres más prestigiosos de la Blockchain. Con el tiempo, la exclusividad de un NOD se traducirá en una dirección de monedero más prestigiosa que cualquier otra en la Blockchain, del mismo modo que una dirección de ciudad de nivel 1 lo ha hecho en el mundo físico.',
+  },
+  "advantages-item2-title": {
+    en: 'Fair Pricing',
+    cn: 'Faire Preisgestaltung',
+    es: 'Precios justos',
+  },
+  "advantages-text2": {
+    en: `NODs are not priced by traits, as for most standard NFTs - they are priced by supply and industrial demand, as in the real world economy. This means that for most NODs, the cost is a standard $9 (plus 70% discount on multiple years' registration if purchased upfront), making BDNS NFTs the most fairly priced to mint around today`,
+    cn: 'Die Preise für NODs werden nicht wie bei den meisten Standard-NFTs nach Merkmalen berechnet, sondern nach Angebot und industrieller Nachfrage, wie in der realen Weltwirtschaft. Das bedeutet, dass die meisten NODs standardmäßig 9 $ kosten (plus 70 % Rabatt auf die Registrierung für mehrere Jahre, wenn sie im Voraus gekauft werden), was BDNS NFTs zu den preisgünstigsten NFTs macht, die es derzeit gibt.',
+    es: 'Los NODs no tienen un precio por rasgos, como la mayoría de los NFTs estándar - tienen un precio por oferta y demanda industrial, como en la economía del mundo real. Esto significa que para la mayoría de los NODs, el coste es de 9$ estándar (más un 70% de descuento en el registro de varios años si se adquiere por adelantado), lo que hace que los NFTs de la BDNS tengan el precio más justo para acuñar hoy en día.',
+  },
+  "advantages-item3-title": {
+    en: 'First Come, First Serve Policy',
+    cn: 'Wer zuerst kommt, mahlt zuerst',
+    es: 'Política de "primer llegado, primer servido',
+  },
+  "advantages-text3": {
+    en: 'There is no private sale and no privileged minting of NODs that would otherwise disadvantage customers of BDNS, except in a very limited number of instances (such as with select brand names, which are reserved aside for a period for their owners). BDNS operates exclusively on a first-come, first-serve minting policy. Get minting before others grab your preferred address!',
+    cn: 'Es gibt keinen privaten Verkauf und keine privilegierte Prägung von NODs, die Kunden von BDNS benachteiligen würde, außer in einer sehr begrenzten Anzahl von Fällen (wie bei ausgewählten Markennamen, die für eine bestimmte Zeit für ihre Besitzer reserviert sind). BDNS arbeitet ausschließlich nach dem Prinzip "Wer zuerst kommt, mahlt zuerst". Prägen Sie, bevor andere sich Ihre bevorzugte Adresse schnappen!',
+    es: 'No existe la venta privada ni la acuñación privilegiada de NODs que  perjudicaría a los clientes de  BDNS, excepto en un número muy limitado de casos (como con marcas selectas, que se reservan durante un periodo para sus propietarios). La BDNS funciona exclusivamente con una política de acuñación por orden de llegada. ¡Consiga la acuñación antes de que otros se hagan con su dirección preferida!',
+  },
+  "advantages-item4-title": {
+    en: 'BDNS Is Building For The Long Term',
+    cn: 'BDNS baut auf lange Sicht',
+    es: 'BDNS construye a largo plazo',
+  },
+  "advantages-text4": {
+    en: `Most NFT products involve simply building a virtual token minting factory and leaving the bulk of work to the customer and the market. With BDNS it's different. BDNS developers are actively working on new Blockchain solutions that will be rolled out consistently to reinforce and revolutionize the value of NODs`,
+    cn: 'Bei den meisten NFT-Produkten wird einfach eine virtuelle Token-Prägefabrik gebaut und der Großteil der Arbeit dem Kunden und dem Markt überlassen. Bei BDNS ist das anders. Die BDNS-Entwickler arbeiten aktiv an neuen Blockchain-Lösungen, die kontinuierlich eingeführt werden, um den Wert von NODs zu stärken und zu revolutionieren.',
+    es: 'La mayoría de los productos de NFT se limitan a construir una fábrica virtual de acuñación de fichas y dejan el grueso del trabajo al cliente y al mercado. Con BDNS es diferente. Los desarrolladores de BDNS están trabajando activamente en nuevas soluciones de Blockchain que se desplegarán de forma consistente para reforzar y revolucionar el valor de los NODs.',
+  },
+  "advantages-item5-title": {
+    en: 'Read, Connect, Mint',
+    cn: 'Lesen, Verbinden, Prägen',
+    es: 'Leer, Conectar, Mint',
+  },
+  "advantages-text5": {
+    en: 'You can find more detailed technical and tokenomics analysis in the BNDS White Paper or you can contact us at contact@bdns.app with any questions. We love to hear from our customers! Otherwise, get minting your exclusive blockchain-based name-only domain right now by searching your preferred name here',
+    cn: 'Eine detaillierte technische und tokenomische Analyse finden Sie im BDNS-Whitepaper, oder Sie können uns bei Fragen unter contact@bdns.app kontaktieren. Wir freuen uns, von unseren Kunden zu hören! Andernfalls können Sie jetzt Ihre exklusive Blockchain-basierte Namensdomain prägen, indem Sie hier Ihren Wunschnamen suchen.',
+    es: 'Puede encontrar un análisis técnico y tokenómico más detallado en el Libro Blanco de BNDS o puede ponerse en contacto con nosotros en contact@bdns.app con cualquier pregunta. ¡Nos encanta escuchar a nuestros clientes! Do lo contrario, empiece a acuñar su dominio exclusivo basado en blockchain ahora mismo buscando su nombre preferido aquí.',
+  },
+};
--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,21 @@
 $(document).ready(function () {
-  // $(".lang").on("click", function () {
-  //   $(this).children().slideToggle();
-  // });
-
-  // $("#selectBox").selectbox();
-
   $("#burger").on("click", function () {
     $(this).toggleClass("active");
     $(".main-nav").toggleClass("active");
+    $('body').toggleClass('hidden');
   });
 
-  $('.main-nav__link').on('click', function() {
-    $("#burger").removeClass('active');
+  $(".main-nav__link").on("click", function () {
+    $('body').removeClass('hidden');
+    $("#burger").removeClass("active");
     $(".main-nav").removeClass("active");
-  })
-
-  $(function () {
-    $('a[href^="#"]').on("click", function (evt) {
-      evt.preventDefault();
-      var sc = $(this).attr("href"),
-        dn = $(sc).offset().top;
-      $("html, body").animate({ scrollTop: dn }, 1000);
-    });
   });
+
+  $('a[href^="#"]').on("click", function (evt) {
+    evt.preventDefault();
+    var sc = $(this).attr("href"),
+      dn = $(sc).offset().top;
+    $("html, body").animate({ scrollTop: dn }, 1000);
+  });
+
 });
